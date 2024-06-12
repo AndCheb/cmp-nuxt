c8851a5816d08528710e0dc2685d00ad95a1aa55
Andrey Cheban <121983233+AndCheb@users.noreply.github.com>
2024-06-12 19:50:37 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
 import { resolve } from 'path';
 
 export default defineNuxtConfig({
+  ssr: true,
   devtools: { enabled: true },
   alias: {
     '@': resolve(__dirname, '/'),
   },
   css: ['~/scss/main.scss'],
   router: {
-    base: '/https://github.com/AndCheb/cmp-nuxt/'
+    base: '/cmp-nuxt/'
+  },
+  static: {
+    prefix: false
   },
 });
